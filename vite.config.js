7e1d26dcfde8083e718fd9140347823036962e1b
nitzanbs <nitzanbs2248@gmail.com>
2024-07-11 01:40:20 +0300

    'Deploy'

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], 
+  base: '/resqeat/', // החליפי 'resqeat' בשם המאגר שלך ב-GitHub
+  build: {
+    outDir: 'docs'}
 })
+

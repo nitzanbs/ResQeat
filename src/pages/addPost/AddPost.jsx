580f95339a1afb8574a367ef878e438f6224cd01
nitzanbs <nitzanbs2248@gmail.com>
2024-07-10 21:38:42 +0300

    add post summary

--- src/pages/addPost/AddPost.jsx
+++ src/pages/addPost/AddPost.jsx
@@ -75,6 +75,7 @@ function AddPost(props) {
   const [ErrMsg, setErrMsg] = useState("");
   const [userLocation, setUserLocation] = useState(null);
   const [selectedDate, setSelectedDate] = useState(null);
+  const [postSubmitted, setPostSubmitted] = useState(false);
   const { image, setImage } = props;
 
   const getLocation = () => {
@@ -100,11 +101,19 @@ function AddPost(props) {
 
   const postCardCollectionRef = collection(dataBase, "PostCollection");
 
+  // const changeHandler = (e) => {
+  //   postCard[e.target.name] = e.target.value;
+  //   console.log(postCard);
+  //   setPostCard({ ...postCard });
+  // };
+
   const changeHandler = (e) => {
-    postCard[e.target.name] = e.target.value;
-    console.log(postCard);
-    setPostCard({ ...postCard });
+    setPostCard({
+      ...postCard,
+      [e.target.name]: e.target.value,
+    });
   };
+
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -117,24 +126,25 @@ function AddPost(props) {
         userName: user.email,
       });
       console.log({ postCardDoc });
-  
+
       // Get the new document
       const postRef = doc(dataBase, "PostCollection", postCardDoc.id);
       const newDoc = await getDoc(postRef);
       setPostCard({ ...newDoc.data(), id: newDoc.id });
+      setPostSubmitted(true);
     } catch (error) {
       setErrMsg("Something went wrong");
     }
   };
-  
+
 
   const handleTimeChange = (newValue) => {
     setSelectedDate(newValue);
     const formattedTime = dayjs(newValue).format();
     setPostCard({ ...postCard, pickupTime: formattedTime });
   };
-  
-  
+
+
   const preset_key = "ml_default";
   const cloud_name = "djmlunvsl";
 
@@ -173,123 +183,146 @@ function AddPost(props) {
         <img className="addPostImageTitle" src="images\Add post.png" alt="" />
       </div>
 
-      <form onChange={changeHandler}>
-        <div className="containerAddPost">
-          <TextField
-            id="standard-basic"
-            label="dish name"
-            name="dishName"
-            variant="standard"
-          />
-          {/* <TextField
+      {!postSubmitted && ( // Show form if post is not submitted
+        <form onChange={changeHandler}>
+          <div className="containerAddPost">
+            <TextField
+              id="standard-basic"
+              label="dish name"
+              name="dishName"
+              variant="standard"
+            />
+            {/* <TextField
             id="standard-basic"
             label="pickup time"
             name="pickupTime"
             variant="standard"
           /> */}
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer components={['TimePicker']}>
-              <TimePicker
-                label="pickup time"
-                value={selectedDate}
-                onChange={handleTimeChange}
-              />
-            </DemoContainer>
-          </LocalizationProvider>
-
-          <Box
-            component="form"
-            sx={{
-              "& .MuiTextField-root": { m: 1, width: "25ch" },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <div className="selectContainer">
-              <TextField
-                onChange={changeHandler}
-                name="amount"
-                id="standard-select-currency"
-                select
-                label="amount by dishes"
-                defaultValue="1"
-                helperText="Please select amount"
-                variant="standard"
-              >
-                {amount.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </TextField>
-
-              <TextField
-                onChange={changeHandler}
-                name="category"
-                id="standard-select-currency"
-                select
-                label="category"
-                defaultValue="All"
-                helperText="Please select your category"
-                variant="standard"
-              >
-                {category.map((option) => (
-                  <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                  </MenuItem>
-                ))}
-              </TextField>
-            </div>
-          </Box>
-          <Box
-            sx={{
-              width: 500,
-              maxWidth: "100%",
-            }}
-          >
-            <TextField fullWidth label="notes" name="notes" id="fullWidth" />
-          </Box>
-
-          <button type="button" onClick={getLocation}>
-            Get GPS Location
-          </button>
-          {userLocation && (
-            <p>
-              Latitude: {userLocation.latitude}, Longitude:{" "}
-              {userLocation.longitude}
-            </p>
-          )}
-
-          <div className="input-group">
-            <label htmlFor="image"></label>
-            <br />
-            <input
-              type="file"
-              accept="image/*"
-              id="image"
-              name="image"
-              onChange={handleFileChange}
-            />
-            <label htmlFor="image" className="fileLabel">
-              <div className="imageInputDiv">
-                <img
-                  className="image"
-                  src="https://cdn.pixabay.com/photo/2017/11/10/04/47/image-2935360_640.png"
-                  alt=""
-                  srcset=""
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoContainer components={['TimePicker']}>
+                <TimePicker
+                  label="pickup time"
+                  value={selectedDate}
+                  onChange={handleTimeChange}
                 />
-                {/* <span className='imageInputText'>Upload Picture</span> */}
+              </DemoContainer>
+            </LocalizationProvider>
+
+            <Box
+              component="form"
+              sx={{
+                "& .MuiTextField-root": { m: 1, width: "25ch" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <div className="selectContainer">
+                <TextField
+                  onChange={changeHandler}
+                  name="amount"
+                  id="standard-select-currency"
+                  select
+                  label="amount by dishes"
+                  defaultValue="1"
+                  helperText="Please select amount"
+                  variant="standard"
+                >
+                  {amount.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+
+                <TextField
+                  onChange={changeHandler}
+                  name="category"
+                  id="standard-select-currency"
+                  select
+                  label="category"
+                  defaultValue="All"
+                  helperText="Please select your category"
+                  variant="standard"
+                >
+                  {category.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
               </div>
-            </label>
+            </Box>
+            <Box
+              sx={{
+                width: 500,
+                maxWidth: "100%",
+              }}
+            >
+              <TextField fullWidth label="notes" name="notes" id="fullWidth" />
+            </Box>
+
+            <button type="button" onClick={getLocation}>
+              Get GPS Location
+            </button>
+            {userLocation && (
+              <p>
+                Latitude: {userLocation.latitude}, Longitude:{" "}
+                {userLocation.longitude}
+              </p>
+            )}
+
+            <div className="input-group">
+              <label htmlFor="image"></label>
+              <br />
+              <input
+                type="file"
+                accept="image/*"
+                id="image"
+                name="image"
+                onChange={handleFileChange}
+              />
+              <label htmlFor="image" className="fileLabel">
+                <div className="imageInputDiv">
+                  <img
+                    className="image"
+                    src="https://cdn.pixabay.com/photo/2017/11/10/04/47/image-2935360_640.png"
+                    alt=""
+                    srcset=""
+                  />
+                  {/* <span className='imageInputText'>Upload Picture</span> */}
+                </div>
+              </label>
+            </div>
+            <img className="addPostImg" src={image} alt="" />
           </div>
-          <img className="addPostImg" src={image} alt="" />
+          <div className="BtnForPostDiv">
+            <button className="BtnForPost" onClick={submitHandler} type="submit">
+              post
+            </button>
+          </div>
+        </form>
+      )}
+      {postSubmitted && (
+        <>
+        <h2>Post submitted successfully!</h2>
+        <div className="summaryScreen">    
+          <h2>Post Summary:</h2>
+          <div>
+          <p>Dish Name: {postCard.dishName}</p>
+          <p>Pickup Time: {dayjs(postCard.pickupTime).format('YYYY-MM-DD HH:mm')}</p>
+          <p>Amount: {postCard.amount}</p>
+          <p>Category: {postCard.category}</p>
+          <p>Notes: {postCard.notes}</p>
+          </div>
+          <p><img className="imageSummary" src={postCard.image} alt="Post Image" /></p>
+
+
         </div>
-        <div className="BtnForPostDiv">
-          <button className="BtnForPost" onClick={submitHandler} type="submit">
-            post
+                  <button className="BtnForPost" onClick={() => setPostSubmitted(false)}>
+            New post 
           </button>
-        </div>
-      </form>
+          </>
+      )}
     </div>
   );
 }
